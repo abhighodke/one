7574bd435f673d5ed578ede68a1e0353b08e06b6
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-20 23:20:04 +0530

    published to github

---
+++ src/clientData.js
@@ -0,0 +1,42 @@
+// src/clientData.js
+const clientData = {
+    headerInitials: "TM", // Header Initials
+    heroTagline: "Tania Michaels Realty", // Hero Tagline
+    agentName: "Tania Michaels",
+    aboutMeShort: "Tania Michaels is a seasoned real estate expert with over two decades of experience. She began her career in mortgages and transitioned into land acquisitions and commercial property negotiations. Known for her expertise in both traditional and short sales, Tania is a top-producing agent with numerous awards and recognitions.", // AboutMe Short Description
+    aboutMeDetailed: `Tania Michaels embarked on her real estate journey in 1993, initially in the mortgage industry. In 2003, she caught the attention of a major land developer, who recruited her for land acquisitions. During her tenure at Focus Commercial Group, Tania brokered over 2,000 acres of land, totaling $215 million.
+
+Her role at Focus Commercial Group involved collaboration with a team of in-house planners, zoning attorneys, and engineers. When the Great Recession hit, Tania shifted her focus to negotiating short sales on commercial properties in default, a transition that later extended to residential properties. Having started her real estate career in the mortgage industry, Tania, a nationally recognized mortgage broker, brought a wealth of knowledge about the banking maze and nearly two decades of real estate negotiation experience to the short sale market.
+
+Tania quickly emerged as a prominent figure in the residential real estate market, excelling as a top-producing agent for both traditional and default property sales. Recognitions followed, including being named:
+
+100 Most Influential Real Estate Agents in Nevada, Real Estate Executive Magazine
+Top 100 Women Real Estate Agents in Las Vegas, Vegas Magazine
+Gary Keller's Top 100 Agents Mastermind
+Greatest Real Estate Moguls, Real Vegas Magazine
+Tania Michaels has successfully closed hundreds of traditional real estate sales, solidifying her reputation as a leader in the field.`, // AboutMe Detailed Description
+    testimonials: [{ quote: "Working with Tania was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
+    { quote: "Tania provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
+    { quote: "The dedication and expertise of Tania were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
+    { quote: "Tania and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
+    { quote: "If you’re looking for a real estate agent who goes above and beyond, Tania is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
+
+
+    ], // Testimonials
+    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
+    email: "support@michaelsTeam.com", // Email
+    address: "10000 W Charleston Blvd, Suite 130 Las Vegas, NV 89135", // Address
+    phone: "702-675-8211", // Phone Number
+    footerDescription: "Tania Michael's Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+    socialMediaLinks: {
+        facebook: "",
+        Instagram: "",
+        linkedin: "",
+    }, // Social Media Links
+    contactDetails: {
+        email: "support@michaelsTeam.com",
+        phone: "702-675-8211",
+    }, // Contact Details
+};
+
+export default clientData;
