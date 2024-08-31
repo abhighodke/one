b69c8cdb50ff83968dcd41def298674f716da6d4
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:34:18 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,42 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "TM", // Header Initials
-    heroTagline: "Tania Michaels Realty", // Hero Tagline
-    agentName: "Tania Michaels",
-    aboutMeShort: "Tania Michaels is a seasoned real estate expert with over two decades of experience. She began her career in mortgages and transitioned into land acquisitions and commercial property negotiations. Known for her expertise in both traditional and short sales, Tania is a top-producing agent with numerous awards and recognitions.", // AboutMe Short Description
-    aboutMeDetailed: `Tania Michaels embarked on her real estate journey in 1993, initially in the mortgage industry. In 2003, she caught the attention of a major land developer, who recruited her for land acquisitions. During her tenure at Focus Commercial Group, Tania brokered over 2,000 acres of land, totaling $215 million.
+    headerInitials: "AB",
+    heroTagline: "Angela Butcher Realty",
+    agentName: "Angela Butcher",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
+    aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
-Her role at Focus Commercial Group involved collaboration with a team of in-house planners, zoning attorneys, and engineers. When the Great Recession hit, Tania shifted her focus to negotiating short sales on commercial properties in default, a transition that later extended to residential properties. Having started her real estate career in the mortgage industry, Tania, a nationally recognized mortgage broker, brought a wealth of knowledge about the banking maze and nearly two decades of real estate negotiation experience to the short sale market.
+With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
-Tania quickly emerged as a prominent figure in the residential real estate market, excelling as a top-producing agent for both traditional and default property sales. Recognitions followed, including being named:
-
-100 Most Influential Real Estate Agents in Nevada, Real Estate Executive Magazine
-Top 100 Women Real Estate Agents in Las Vegas, Vegas Magazine
-Gary Keller's Top 100 Agents Mastermind
-Greatest Real Estate Moguls, Real Vegas Magazine
-Tania Michaels has successfully closed hundreds of traditional real estate sales, solidifying her reputation as a leader in the field.`, // AboutMe Detailed Description
-    testimonials: [{ quote: "Working with Tania was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", author: "John and Emily R." },
-    { quote: "Tania provided exceptional service from start to finish. The team’s professionalism and attention to detail ensured that our property sale was smooth and successful. We couldn’t have asked for a better partner in real estate.", author: "Michael S." },
-    { quote: "The dedication and expertise of Tania were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", author: "Sarah and David L." },
-    { quote: "Tania and her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", author: "Jessica and Mark T." },
-    { quote: "If you’re looking for a real estate agent who goes above and beyond, Tania is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", author: "Laura K." }
-
-
-    ], // Testimonials
-    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
-    email: "support@michaelsTeam.com", // Email
-    address: "10000 W Charleston Blvd, Suite 130 Las Vegas, NV 89135", // Address
-    phone: "702-675-8211", // Phone Number
-    footerDescription: "Tania Michael's Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+Customer Service excellence is our Passion!`,
+    testimonials: [{"quote": "Working with Angela Butcher was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Angela Butcher provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Angela Butcher were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Angela Butcher and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Angela Butcher is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    footerTagline: "Your Trusted Partner in Luxury Real Estate",
+    email: "angelabtherealtor@gmail.com",
+    address: "1550 W 2nd St Ste A2 , Gulf Shores, AL, United States",
+    phone: "251-654-9824",
+    footerDescription: `Angela Butcher Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "",
+        facebook: "https://www.facebook.com/HomesbyAngelaB",
         Instagram: "",
         linkedin: "",
-    }, // Social Media Links
+    },
     contactDetails: {
-        email: "support@michaelsTeam.com",
-        phone: "702-675-8211",
-    }, // Contact Details
+        email: "angelabtherealtor@gmail.com",
+        phone: "251-654-9824",
+    },
+    website: "",
+    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/421671424_342298018639287_3894760131471092657_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=brO5lhvJBfsQ7kNvgHxBdk3&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYB-rWTyifjTxE2UfwgVmr649Y6xhxhAVjEiJyROlLz30g&oe=66D7D7FD" // Agent image URL
 };
 
 export default clientData;
