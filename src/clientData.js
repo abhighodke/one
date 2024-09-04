6e2107022a6ae4b2cad54c4eee21c567dccd2c4c
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:36:42 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "AB",
-    heroTagline: "Angela Butcher Realty",
-    agentName: "Angela Butcher",
+    headerInitials: "LS",
+    heroTagline: "Lauren Sazera Realty",
+    agentName: "Lauren Sazera",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Angela Butcher was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Angela Butcher provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Angela Butcher were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Angela Butcher and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Angela Butcher is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Lauren Sazera was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Lauren Sazera provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Lauren Sazera were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Lauren Sazera and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Lauren Sazera is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "angelabtherealtor@gmail.com",
-    address: "1550 W 2nd St Ste A2 , Gulf Shores, AL, United States",
-    phone: "251-654-9824",
-    footerDescription: `Angela Butcher Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "lsazera@kw.com",
+    address: "3595 Grandview Pkwy, Ste 250, Birmingham, AL, United States",
+    phone: "205-901-4955",
+    footerDescription: `Lauren Sazera Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/HomesbyAngelaB",
+        facebook: "https://www.facebook.com/laurensazerarealtor/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "angelabtherealtor@gmail.com",
-        phone: "251-654-9824",
+        email: "lsazera@kw.com",
+        phone: "205-901-4955",
     },
     website: "",
-    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/421671424_342298018639287_3894760131471092657_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=brO5lhvJBfsQ7kNvgHxBdk3&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYB-rWTyifjTxE2UfwgVmr649Y6xhxhAVjEiJyROlLz30g&oe=66D7D7FD" // Agent image URL
+    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/332240342_529079999352907_2161192938035321362_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=NGOqecCOhL8Q7kNvgE0ObEU&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYBvVK8jt8pP_NvU3kbTA4if0t0NZAzaLD88supqL7EbEg&oe=66DBCC2B" // Agent image URL
 };
 
 export default clientData;
